3e50cae62cc0295554453e7354c1789e1b7dad73
hshioura@gmail.com <hshioura@gmail.com>
2019-04-20 21:44:17 +0900

    first commit

---
+++ app/src/main/java/org/example/stopwatch/MainActivity.kt
@@ -0,0 +1,66 @@
+package org.example.stopwatch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    val handler = Handler()
+    var timeValue = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = start
+        val stopButton = stop
+        val resetButton = reset
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+
+                timeToText(timeValue)?.let {
+                    timeText.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+
+        startButton.setOnClickListener {
+            handler.post(runnable)
+        }
+
+        stopButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+
+        resetButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = 0
+            timeToText()?.let {
+                timeText.text = it
+            }
+        }
+    }
+
+
+    private fun timeToText(time: Int = 0): String? {
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m, s)
+        }
+    }
+}
